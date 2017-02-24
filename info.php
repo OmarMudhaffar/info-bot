a27d147220e1541410f72c9f11b6c8b6d2ea46dd
omar-bad <real25130@gmail.com>
2017-02-25 02:02:15 +0300

    TEAM-REAL

---
+++ info.php
@@ -0,0 +1,34 @@
+<?php
+$botToken = "365151846:AAFrWYBF4__i03yPpkmqvnnxGtwGb16CS4M";
+$website = "https://api.telegram.org/bot".$botToken;
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+// By OmarReal
+$fwd = $update["message"]["forward_from"];
+$fwd2 = $update["message"]["forward_from"]["id"];
+$fwd = $update["message"]["forward_from"]["message"];
+$fwd4 = $update["message"]["forward_from"]["channel"]["id"];
+$user2 = $update["message"]["forward_from"]["username"];
+$type = $update["message"]["chat"]["type"];
+$for = $update["message"]["from"]["id"];
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+$user = $update["message"]["from"]["username"];
+$nam = $update["message"]["from"]["first_name"];
+$fwd_name = $update["message"]["forward_from"]["first_name"];
+
+if ($message && !$fwd2 && $type == "private"){
+sendMessage($chatId, "💡Id : " . $for . "\n💡User : " . "@" . $user . "\n💡Name : " . $nam);
+}
+if ($fwd2 && $type == "private"){
+sendMessage($chatId, "💡Id : " . $fwd2 . "\n💡user : " . "@" . $user2 . "\n💡Name : " . $fwd_name);	
+}
+function sendMessage ($chatId, $message){
+		
+
+		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+		file_get_contents($url);
+		}
+
+		?>
+		
